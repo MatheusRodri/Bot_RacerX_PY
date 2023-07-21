3fbf5ab7bb11c0b5e369312cbfc27500a7abaa9b
Matheus <matheus.rj25@hotmail.com>
2023-07-21 20:49:43 -0300

    erro na inicialização

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from discord.ext import commands
 import os
 
 TOKEN = token.get("TOKEN")
-intents = discord.Intents.default()
-bot = commands.Bot(command_prefix='/',intents=intents)
 
+intents = discord.Intents.default()
+intents.message_content = True
+intents.typing = True
+
+bot = commands.Bot(command_prefix='/',intents=intents)
 bot.add_cog(commands.Cog(bot))
 
 bot.run(TOKEN)
